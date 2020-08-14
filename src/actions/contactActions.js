3bd8cf2d221fe8ca283d3298b6c537142b662943
rachid3alaoui <rachid3alaoui@gmail.com>
2020-08-14 19:23:11 +0100

    first commit

---
+++ src/actions/contactActions.js
@@ -0,0 +1,65 @@
+import {
+  GET_CONTACTS,
+  GET_CONTACT,
+  ADD_CONTACT,
+  UPDATE_CONTACT,
+  DELETE_CONTACT,
+} from './actionsTypes';
+import axios from 'axios';
+
+export const getContacts = () => async (dispatch) => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+  dispatch({
+    type: GET_CONTACTS,
+    payload: res.data,
+  });
+};
+
+export const getSingleContact = (id) => async (dispatch) => {
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+
+  dispatch({
+    type: GET_CONTACT,
+    payload: res.data,
+  });
+};
+
+export const addContact = (contact) => async (dispatch) => {
+  const res = await axios.post(
+    'https://jsonplaceholder.typicode.com/users',
+    contact
+  );
+  dispatch({
+    type: ADD_CONTACT,
+    payload: res.data,
+  });
+};
+
+export const deleteContact = (id) => async (dispatch) => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id,
+    });
+  } catch (error) {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id,
+    });
+  }
+};
+
+export const updateContact = (contact) => async (dispatch) => {
+  const res = await axios.put(
+    `https://jsonplaceholder.typicode.com/users/${contact.id}`,
+    contact
+  );
+
+  dispatch({
+    type: UPDATE_CONTACT,
+    payload: res.data,
+  });
+};
